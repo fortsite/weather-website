384f54783392c49870cb53e061c5881ddb9f19c9
Sergei Zhukov <szhukov@gmail.com>
2019-05-26 10:30:10 +0400

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `https://api.darksky.net/forecast/b80a6fa614b642cbc93c3e5ee8c4f3e3/${latitude},${longitude}?units=si`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unabel to connect mapbox to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location');
+    } else {
+      const { temperature, precipProbability } = body.currently;
+      const { summary } = body.daily.data[0];
+      callback(
+        undefined,
+        `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% change of rain`
+      );
+    }
+  });
+};
+module.exports = forecast;
